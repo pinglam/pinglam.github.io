555ccb52935ff713c94c58f1ec18fd7099ec6750
pinglam <39413634+pinglam@users.noreply.github.com>
2020-06-16 11:52:38 +0800

    upload existing file for test this website is work

---
+++ app-files/data.js
@@ -0,0 +1,72 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-94146864_2662561730519989_5235952973665796096_n",
+      "name": "94146864_2662561730519989_5235952973665796096_n",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "yaw": -2.5367166505479943,
+        "pitch": 0.09460365888053701,
+        "fov": 1.3715802068843215
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.3723702834246296,
+          "pitch": 0.05745740452581494,
+          "rotation": 0,
+          "target": "1-93024908_705099043567017_8504990932137934848_n"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-93024908_705099043567017_8504990932137934848_n",
+      "name": "93024908_705099043567017_8504990932137934848_n",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
